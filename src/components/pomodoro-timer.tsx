9f942fa03b37bcfb0e59f4b21f598d8d8d8a828f
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-23 18:21:55 -0300

    Convertendo milisegundos em segundos

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useInterval } from '../hooks/useInterval';
+import { secondsToTime } from '../utils/seconds-to-time';
 
 interface Props {
     defaultPomodoroTime: number;
@@ -9,8 +10,8 @@ export function PomodoroTimer(props: Props): JSX.Element {
     const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
 
     useInterval(() => {
-        setMainTime(mainTime + 1);
+        setMainTime(mainTime - 1);
     }, 1000);
 
-    return <div>Olá mundo! {mainTime}</div>;
+    return <div>Olá mundo! {secondsToTime(mainTime)}</div>;
 }
